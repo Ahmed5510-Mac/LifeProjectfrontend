7b0d696a75d73dbf33f499acc02672b5eba2af23
nada-medhat229 <nda96642@gmail.com>
2022-04-08 01:36:24 +0200

    add about page and footer comp

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,144 @@
+import img2 from "../../assets/img2.jpg";
 const Footer = () => {
-    return (<>footer</>);
-}
+  return (
+    <>
+      <div className="container">
+        <footer className="py-5">
+          <div className="row">
+         
+            <div className="col-md-3 col-sm-12">
+                <div className="d-flex  align-items-center">
+                <img src={img2} alt="" className="rounded-circle" />
+              <h5 className="ms-3">LIFE</h5>
+                </div>
+          
+           
+              <ul className="nav flex-column mt-3">
+                <li className="nav-item mb-2">
+                  <a href="#/" className="nav-link p-0 text-muted">
+                    <i className="fas fa-home mr-3"></i> New York, NY 10012, US
+                  </a>
+                </li>
+                <li className="nav-item mb-2">
+                  <a href="#/" className="nav-link p-0 text-muted">
+                    <i className="fas fa-envelope mr-3"></i> info@example.com
+                  </a>
+                </li>
+                <li className="nav-item mb-2">
+                  <a href="#/" className="nav-link p-0 text-muted">
+                    <i className="fas fa-print mr-3"></i> + 01 234 567 89
+                  </a>
+                </li>
+                <li className="nav-item mb-2">
+                  <a href="#/" className="nav-link p-0 text-muted">
+                    <i className="fa-solid fa-location-dot"></i> 123 Great
+                    Street Cairo | Egypt
+                  </a>
+                </li>
+               
+              </ul>
+            </div>
+
+            <div className="col-md-3 col-sm-12 mt-5">
+              <h5>Support</h5>
+              <ul className="nav flex-column mt-3">
+                <li className="nav-item mb-2">
+                  <a href="#/" className="nav-link p-0 text-muted">
+                   Help Center
+                  </a>
+                </li>
+                <li className="nav-item mb-2">
+                  <a href="#/" className="nav-link p-0 text-muted">
+                   My Orders
+                  </a>
+                </li>
+                <li className="nav-item mb-2">
+                  <a href="#/" className="nav-link p-0 text-muted">
+                    Account Restore 
+                  </a>
+                </li>
+                <li className="nav-item mb-2">
+                  <a href="#/" className="nav-link p-0 text-muted">
+                  Hire an Expert
+                  </a>
+                </li>
+              </ul>
+            </div>
+
+            <div className="col-md-6 col-sm-12 ">
+              <form>
+                <h4 className="footer_title text-center ">
+                  Contact Us
+                </h4>
+
+                <div className="d-flex form-group mb-2 gap-2">
+                  <input
+                    type="text"
+                    name="contact[fullname]"
+                    className="form-control"
+                    placeholder="Your name"
+                    required=""
+                  />
+                  <input
+                    type="text"
+                    name="contact[tel]"
+                    className="form-control"
+                    placeholder="Your Phone"
+                    required=""
+                  />
+                </div>
+                <div className="form-group mb-2">
+                  <input
+                    name="email"
+                    className="form-control"
+                    placeholder="Your email"
+                    pattern="[A-Za-z0-9]+([_\.][A-Za-z0-9]+)*@([A-Za-z0-9\-]+\.)+[A-Za-z]{2,6}"
+                    required=""
+                  />
+                </div>
+                <div className="form-group mb-2">
+                  <textarea
+                    className="form-control"
+                    rows="3"
+                    name="message"
+                    placeholder="Message"
+                    required=""
+                    minLength="5"
+                  ></textarea>
+                </div>
+                <div className="form-group ">
+                  <button className="btn btn-primary w-100" type="button">
+                    Subscribe
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+
+          <div className="d-flex justify-content-between py-4 my-4 border-top">
+            <p>© 2022 life, All rights reserved.</p>
+            <ul className="list-unstyled d-flex">
+              <li className="ms-3">
+                <a className="link-dark" href="#/">
+                  <i className="fa-brands fa-twitter"></i>
+                </a>
+              </li>
+              <li className="ms-3">
+                <a className="link-dark" href="#/">
+                  <i className="fa-brands fa-instagram"></i>
+                </a>
+              </li>
+              <li className="ms-3">
+                <a className="link-dark" href="#/">
+                  <i className="fa-brands fa-facebook"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </footer>
+      </div>
+    </>
+  );
+};
 
 export default Footer;
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,5 +1,98 @@
+import img3 from "../../assets/img3.jpg";
+import img4 from "../../assets/img4.jpg";
+import img5 from "../../assets/img5.jpg";
+import img6 from "../../assets/img6.jpg";
+import img7 from "../../assets/img7.jpg";
+import img1 from "../../assets/img1.jpg";
+import Card from "../../components/Card/Card";
+
 const About = () => {
-    return ( <>about</> );
-}
- 
+  return (
+    <>
+      <div className="container">
+
+
+        <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img src={img3} className="d-block w-100" alt="..."/>
+    </div>
+    <div className="carousel-item">
+      <img src={img4} className="d-block w-100" alt="..."/>
+    </div>
+    <div className="carousel-item">
+      <img src={img3} className="d-block w-100" alt="..."/>
+    </div>
+  </div>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+   
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    
+  </button>
+</div>
+
+
+
+
+
+          <div>
+        <h1>Who We Are</h1>
+        <p>
+          development and production of dental curing light, dental chair
+          control system, dental burs, dental led lamp, dental endo products and
+          so on of the company. The company has rich experience
+          capable sales team and strong manufacturing strength. Since its
+          establishment it has been continuously developing new products. In the
+          future, we will continue to work hard and innovate constantly. We will
+          uphold our mission: Always working for better quality and better life!
+        </p>
+        </div>
+        <div>
+          <h2>our team</h2>
+
+          <div className="row">
+            <div className="col-md-6 col-sm-12">
+              <Card
+                name="Research"
+                description=" We have our own engineers for both software and hardware why ODM and OEM service are welcome for us."
+                image={img5}
+              />
+            </div>
+            <div className="col-md-6 col-sm-12">
+             
+              <Card
+                name=" Development "
+                description=" We have our own engineers for both software and hardware why ODM and OEM service are welcome for us."
+                image={img6}
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-6 col-sm-12">
+              
+              <Card
+                name="Development"
+                description=" We have our own engineers for both software and hardware why ODM and OEM service are welcome for us."
+                image={img7}
+              />
+            </div>
+            <div className="col-md-6 col-sm-12">
+              
+              <Card
+                image={img1}
+                name="Sales department"
+                description=" Stable and high efficiency sales group are very 
+                 communicating skills and rich experience"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
 export default About;
