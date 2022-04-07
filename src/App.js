4f76cdec3e2722fb1b14ca0a9b41f570768a90c2
nada-medhat229 <nda96642@gmail.com>
2022-04-08 01:47:37 +0200

    add assets

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ const App = () => {
             <Route path='*' element={<NotFound />} />
           </Routes>
         </BrowserRouter>
- {/*  <Footer/>  */}
+  <Footer/> 
 
       </ThemeProvider>
     </>
