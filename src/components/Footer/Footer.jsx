3d2682e9415d90aa0bf2d763cf7542bf1456e031
nada-medhat229 <nda96642@gmail.com>
2022-04-10 02:19:26 +0200

    update color in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,12 @@
 import img2 from "../../assets/img2.jpg";
+import styles from './footer.module.css'
+
 const Footer = () => {
   return (
     <>
-      <div className="container">
-        <footer className="py-5">
-          <div className="row">
+      <div className={styles.footerbg}>
+        <footer className="container py-5 " >
+          <div className="row m-0 p-1">
          
             <div className="col-md-3 col-sm-12">
                 <div className="d-flex  align-items-center">
@@ -13,24 +15,24 @@ const Footer = () => {
                 </div>
           
            
-              <ul className="nav flex-column mt-3">
-                <li className="nav-item mb-2">
-                  <a href="#/" className="nav-link p-0 text-muted">
-                    <i className="fas fa-home mr-3"></i> New York, NY 10012, US
+              <ul className="nav flex-column mt-3" >
+                <li className="nav-item mb-2 ">
+                  <a href="#/" className="nav-link p-0 link-light">
+                    <i className="fas fa-home mr-3" ></i> New York, NY 10012, US
                   </a>
                 </li>
                 <li className="nav-item mb-2">
-                  <a href="#/" className="nav-link p-0 text-muted">
+                  <a href="#/" className="nav-link p-0 link-light">
                     <i className="fas fa-envelope mr-3"></i> info@example.com
                   </a>
                 </li>
                 <li className="nav-item mb-2">
-                  <a href="#/" className="nav-link p-0 text-muted">
+                  <a href="#/" className="nav-link p-0 link-light">
                     <i className="fas fa-print mr-3"></i> + 01 234 567 89
                   </a>
                 </li>
                 <li className="nav-item mb-2">
-                  <a href="#/" className="nav-link p-0 text-muted">
+                  <a href="#/" className="nav-link p-0 link-light">
                     <i className="fa-solid fa-location-dot"></i> 123 Great
                     Street Cairo | Egypt
                   </a>
@@ -43,24 +45,24 @@ const Footer = () => {
               <h5>Support</h5>
               <ul className="nav flex-column mt-3">
                 <li className="nav-item mb-2">
-                  <a href="#/" className="nav-link p-0 text-muted">
+                 
                    Help Center
-                  </a>
+                 
                 </li>
                 <li className="nav-item mb-2">
-                  <a href="#/" className="nav-link p-0 text-muted">
+                  
                    My Orders
-                  </a>
+                  
                 </li>
                 <li className="nav-item mb-2">
-                  <a href="#/" className="nav-link p-0 text-muted">
+                 
                     Account Restore 
-                  </a>
+                  
                 </li>
                 <li className="nav-item mb-2">
-                  <a href="#/" className="nav-link p-0 text-muted">
+                  
                   Hire an Expert
-                  </a>
+                  
                 </li>
               </ul>
             </div>
@@ -115,21 +117,21 @@ const Footer = () => {
             </div>
           </div>
 
-          <div className="d-flex justify-content-between py-4 my-4 border-top">
+          <div className="d-flex justify-content-between p-4 my-4 border-top">
             <p>© 2022 life, All rights reserved.</p>
             <ul className="list-unstyled d-flex">
               <li className="ms-3">
-                <a className="link-dark" href="#/">
+                <a className="link-light" href="#/">
                   <i className="fa-brands fa-twitter"></i>
                 </a>
               </li>
               <li className="ms-3">
-                <a className="link-dark" href="#/">
+                <a className="link-light" href="#/">
                   <i className="fa-brands fa-instagram"></i>
                 </a>
               </li>
               <li className="ms-3">
-                <a className="link-dark" href="#/">
+                <a className="link-light" href="#/">
                   <i className="fa-brands fa-facebook"></i>
                 </a>
               </li>
