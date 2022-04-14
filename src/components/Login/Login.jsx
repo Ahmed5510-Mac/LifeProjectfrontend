feb6ef7412e275502af264f3e433131c1a662330
unknown <bishohany4@gmail.com>
2022-04-14 15:55:33 +0200

    update login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,12 +1,3 @@
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import Avatar from '@mui/material/Avatar';
-import TextField from '@mui/material/TextField';
-import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
-import Button from '@mui/material/Button';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
@@ -21,10 +12,8 @@ const Login = ({ handleChange }) => {
     const { isLoading, user, isSuccess, isError, message } = useSelector((state) => state.auth)
 
     const [formData, setFormData] = useState({
-        // customerEmail: '',
-        // customerPassword: '',
-        email:'',
-        password:''
+        email: '',
+        password: ''
     })
 
     useEffect(() => {
@@ -47,8 +36,7 @@ const Login = ({ handleChange }) => {
     const handleSubmit = (e) => {
         e.preventDefault()
         const userData = {
-            // customerEmail, customerPassword
-            email,password
+            email, password
         }
         dispatch(login(userData))
     }
@@ -56,62 +44,22 @@ const Login = ({ handleChange }) => {
         return <Spinner />
     }
 
-    const { email,password} = formData
+    const { email, password } = formData
 
     return (<>
-        <form  className='continer p-5 mt-3 sign-in text-center' onSubmit={handleSubmit}>
+        <form className='continer p-5 mt-3 sign-in text-center' onSubmit={handleSubmit}>
             <div className='d-flex align-items-center justify-content-center w-100 '>
-            <label htmlFor="userName"><i className="fa-solid border p-2 rounded-circle fa-user"></i></label>
-                <input placeholder='userName' className='form-control w-100 ms-2' type="email" name='email' value={email} onChange={onChange} />
+                <label htmlFor="userName"><i className="fa-solid border p-2 rounded-circle fa-user"></i></label>
+                <input required placeholder='userName' className='form-control w-100 ms-2' type="email" name='email' value={email} onChange={onChange} />
             </div>
             <div className='d-flex justify-content-center align-items-center w-100 mt-2' >
-            <label htmlFor="Passoword"><i className="fa-solid fa-unlock-keyhole border p-2 rounded-circle"></i></label>
-                <input placeholder='Passoword' className='form-control  ms-2' type="text" name='password' value={password} onChange={onChange} />
+                <label htmlFor="Passoword"><i className="fa-solid fa-unlock-keyhole border p-2 rounded-circle"></i></label>
+                <input required placeholder='Passoword' className='form-control  ms-2' type="text" name='password' value={password} onChange={onChange} />
             </div>
 
-            <button className='btn btn-lg text-white my-3' type="submit">Lgoin <i class="fa-solid fa-right-to-bracket"></i></button><br/>
-            <a href='#' className="password">Forgot your password ?</a>
+            <button className='btn btn-lg text-white mt-3' type="submit">Login <i class="fa-solid fa-right-to-bracket"></i></button><br />
         </form>
-
-
     </>);
 }
 
 export default Login;
-/*
-    const paperStyle = { padding: 20, height: '70vh', width: 300, margin: '0 auto' }
-    const btnStyle = { margin: '8px 0' }
-    const avatarStyle = { backgroundColor: '#1bbd7e' }
-    return (<>
-        <Grid>
-            <Paper elvation={20} style={paperStyle}>
-                <Grid align="center">
-                    <Avatar style={avatarStyle}></Avatar>
-                    <h2 style={{fontSize:25}}>Sign IN</h2>
-                </Grid>
-                <TextField label="Username" placeholder='Enter username' fullWidth required />
-                <TextField label="Password" placeholder='Enter password' type="password" fullWidth required />
-                <FormControlLabel
-                    control={
-                        <Checkbox
-                            name="checkedB"
-                            color="primary"
-                        />
-                    }
-                    label="Remember me"
-                />
-                <Button type="submit" color="primary" variant="contained" style={btnStyle} fullWidth>Sign in</Button>
-                <Typography>
-                    <Link href="#">
-                        Forget Password ?
-                    </Link>
-                </Typography>
-                <Typography>Do you have an account ?
-                    <Link href="#" onClick={() => handleChange("event", 1)}>
-                        Sign Up
-                    </Link>
-                </Typography>
-            </Paper>
-        </Grid>
-    </>);
-*/
--- src/components/Sign-up/Sign-up.jsx
+++ src/components/Sign-up/Sign-up.jsx
@@ -1,15 +1,3 @@
-import Checkbox from '@mui/material/Checkbox';
-import TextField from '@mui/material/TextField';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
-import Avatar from '@mui/material/Avatar';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormLabel from '@mui/material/FormLabel';
-import FormControl from '@mui/material/FormControl';
-import Button from '@mui/material/Button';
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
 import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
@@ -38,7 +26,8 @@ const SignUP = () => {
 
     useEffect(() => {
         if (isError) {
-            toast.error(message)
+          //  toast.error(message)
+          return'pleasesfle'
         }
         if (isSuccess || user) {
             navigate('/')
@@ -94,31 +83,31 @@ const SignUP = () => {
         <form onSubmit={handleSubmit} className='continer p-5 text-center  mt-3 sign-Up'>
             <div className='d-flex align-items-center justify-content-evenly w-100'>
             <label htmlFor="userName"><i className="fa-solid border p-2 rounded-circle fa-user" role="button"></i></label>
-                <input className='form-control w-75 ' placeholder='Enter Your fullname' type="text" name='fullName' value={fullName} onChange={onChange} />
+                <input required className='form-control w-75 ' placeholder='Enter Your fullname' type="text" name='fullName' value={fullName} onChange={onChange} />
             </div>
             <div className='d-flex align-items-center justify-content-evenly w-100'>
             <label htmlFor="userName"><i class="fa-solid fa-phone border p-2 rounded-circle " role="button"></i></label>
-                <input className='form-control w-75 'type="number" placeholder='Enter Your Phonenumber'  name='customerPhone' value={customerPhone} onChange={onChange} />
+                <input required className='form-control w-75 'type="number" placeholder='Enter Your Phonenumber'  name='customerPhone' value={customerPhone} onChange={onChange} />
             </div>
             <div className='d-flex align-items-center justify-content-evenly w-100'>
             <label htmlFor="userName"><i class="fa-solid fa-at border p-2 rounded-circle  " role="button"></i></label>
-                <input className='form-control w-75 'type="email" placeholder='Enter Your Email' name='customerEmail' value={customerEmail} onChange={onChange} />
+                <input required className='form-control w-75 'type="email" placeholder='Enter Your Email' name='customerEmail' value={customerEmail} onChange={onChange} />
             </div>
             <div className='d-flex align-items-center justify-content-evenly w-100'>
             <label htmlFor="Passoword"><i className="fa-solid fa-unlock-keyhole border p-2 rounded-circle " role="button"></i></label>
-                <input className='form-control w-75 'type="text" placeholder='Enter Your Password' name='customerPassword' value={customerPassword} onChange={onChange} />
+                <input required className='form-control w-75 'type="text" placeholder='Enter Your Password' name='customerPassword' value={customerPassword} onChange={onChange} />
             </div>
             <div className='d-flex align-items-center justify-content-evenly w-100'>
             <label htmlFor="Passoword"><i className="fa-solid fa-unlock-keyhole border p-2 rounded-circle  " role="button"></i></label>
-                <input className='form-control w-75 'type="text" placeholder='Confirm Your Password' name='confirmPassword' value={confirmPassword} onChange={onChange} />
+                <input required className='form-control w-75 'type="text" placeholder='Confirm Your Password' name='confirmPassword' value={confirmPassword} onChange={onChange} />
             </div>
             <div className='d-flex align-items-center justify-content-evenly w-100'>
             <label htmlFor="userName"><i className="fa-solid border p-2 rounded-circle fa-user  " role="button"></i></label>
-                <input className='form-control w-75 'type="text" placeholder='Enter Your Role' name='role' value={role} onChange={onChange} />
+                <input required className='form-control w-75 'type="text" placeholder='Enter Your Role' name='role' value={role} onChange={onChange} />
             </div>
             <div className='d-flex align-items-center justify-content-evenly w-100'>
             <label htmlFor="userName"><i className="fa-solid fa-location-dot border rounded-circle p-2  " role="button"></i></label>
-                <input className='form-control w-75'type="text" placeholder='Enter Your Address' name='customerAddresses' value={customerAddresses} onChange={onChange} />
+                <input required className='form-control w-75'type="text" placeholder='Enter Your Address' name='customerAddresses' value={customerAddresses} onChange={onChange} />
             </div>
             <button className='btnSubmit btn mt-3' type="submit">submit <i class="fa-solid fa-clipboard-list"></i></button>
         </form>
@@ -128,41 +117,3 @@ const SignUP = () => {
 
 export default SignUP;
 
-/*
-  const paperStyle = { padding: 10, width: 300, margin: "0 auto" }
-    const headerStyle = { margin: 0, fontSize: 25 }
-    const avatarStyle = { backgroundColor: '#1bbd7e' }
-    const marginTop = { marginTop: 0 }
----------------------------------
-   <Grid>
-            <Paper style={paperStyle}>
-                <Grid align='center'>
-                    <Avatar style={avatarStyle}>
-
-                    </Avatar>
-                    <h2 style={headerStyle}>Sign Up</h2>
-                    <Typography variant='caption' gutterBottom>Please fill this form to create an account !</Typography>
-                </Grid>
-                <form>
-                    <TextField fullWidth label='Name' placeholder="Enter your name" />
-                    <TextField fullWidth label='Email' placeholder="Enter your email" />
-                    <FormControl component="fieldset" style={marginTop}>
-                        <FormLabel component="legend">Gender</FormLabel>
-                        <RadioGroup aria-label="gender" name="gender" style={{ display: 'initial' }}>
-                            <FormControlLabel value="female" control={<Radio />} label="Female" />
-                            <FormControlLabel value="male" control={<Radio />} label="Male" />
-                        </RadioGroup>
-                    </FormControl>
-                    <TextField fullWidth label='Phone Number' placeholder="Enter your phone number" />
-                    <TextField fullWidth label='Password' placeholder="Enter your password" />
-                    <TextField fullWidth label='Confirm Password' placeholder="Confirm your password" />
-                    <FormControlLabel
-                        control={<Checkbox name="checkedA" />}
-                        label="I accept "
-                    />
-                    <Button type='submit' variant='contained' color='primary'>Sign up</Button>
-                </form>
-            </Paper>
-        </Grid>
-
-*/
